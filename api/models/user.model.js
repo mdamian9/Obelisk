3f4dcb3249ad85142e166beba0d1406895f79530
mdamian9 <m.damian9@hotmail.com>
2020-03-06 01:58:55 -0800

    Completed user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,7 +3,49 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
-    // User Schema
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+// Execute before each user.save() call
+userSchema.pre('save', callback => {
+    let user = this;
+    // Break out if the password hasn't changed
+    if (!user.isModified('password')) return callback();
+    // Hash new password if it has changed
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return callback(err);
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return callback(err);
+            user.password = hash;
+            callback();
+        });
+    });
+});
+
+UserSchema.methods.verifyPassword = (password, callback) => {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', userSchema);
