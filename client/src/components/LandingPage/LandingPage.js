8a316b9f3f769a9e32a6c96cdb39d1cd9e10b9c1
mdamian9 <m.damian9@hotmail.com>
2020-03-17 18:12:17 -0700

    More progress on Landing Page. Adjusted CSS to create a perfectly centered 'welcome' div. Will work on a sign-up button (modal) under the welcome div

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -7,12 +7,22 @@ const LandingPage = () => {
     return (
         <div>
             <LandingNavbar />
-            <Container>
-                <Row>
-                    <Col className='text-center'>
-                    </Col>
-                </Row>
-            </Container>
+            <div id='remaining-div'>
+                <Container>
+                    <Row>
+                        <Col id='welcome'>
+                            <h3>
+                                Welcome to obelisk.trade, where you can practice trading highly volatile cryptocurrencies without
+                                risking real capital.
+                            </h3>
+                            <br />
+                            <h5>
+                                Log in above or create an account below!
+                            </h5>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 };
