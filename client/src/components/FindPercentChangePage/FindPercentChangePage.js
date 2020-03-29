718b960d03512b598027256c9a668ebf733f9314
mdamian9 <m.damian9@hotmail.com>
2020-03-29 14:23:50 -0700

    Progress on FindPercentChangePage - tool works 100% now, added resetCalculation button just to clear every field on the screen if the user wants to start a clean calculation

--- client/src/components/FindPercentChangePage/FindPercentChangePage.js
+++ client/src/components/FindPercentChangePage/FindPercentChangePage.js
@@ -19,6 +19,16 @@ class FindPercentChangePage extends Component {
         };
     };
 
+    resetCalculation = () => {
+        this.setState({
+            tradingPair: '',
+            entryPrice: '0.0',
+            exitPrice: '0.0',
+            percentChange: '0.0'
+        });
+        document.getElementById('calc-form').reset();
+    };
+
     handleChange = event => {
         // Extract name & value from event target and set to state
         const { name, value } = event.target;
@@ -34,7 +44,15 @@ class FindPercentChangePage extends Component {
 
     handleFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state);
+        const { tradingPair, entryPrice, exitPrice } = event.target;
+        const percentChange = ((parseFloat(exitPrice.value) - parseFloat(entryPrice.value)) / parseFloat(entryPrice.value)) * 100;
+        this.setState({
+            tradingPair: tradingPair.value,
+            entryPrice: entryPrice.value,
+            exitPrice: exitPrice.value,
+            percentChange: percentChange.toFixed(2).toString()
+        });
+        event.target.reset();
     };
 
     render = () => {
@@ -50,7 +68,7 @@ class FindPercentChangePage extends Component {
                                     Find Percent Change
                                 </h3>
                                 <hr className='ln-white' />
-                                <Form onSubmit={this.handleFormSubmit}>
+                                <Form id='calc-form' onSubmit={this.handleFormSubmit}>
                                     <FormGroup>
                                         <Label for='trading-pair'>Trading Pair:</Label>
                                         <Input type='select' name='tradingPair' id='trading-pair'
@@ -88,14 +106,20 @@ class FindPercentChangePage extends Component {
                                     </Col>
                                 </Row>
                                 <hr className='ln-white' />
-                                <Row className='text-center'>
+                                <Row className='align-items-center text-center'>
                                     <Col>
                                         Entry price: {this.state.entryPrice} {this.state.tradingPair}
                                         <br />
                                         Exit price: {this.state.exitPrice} {this.state.tradingPair}
                                     </Col>
                                     <Col>
-                                    Percent change: {this.state.percentChange} %
+                                        Percent change: {this.state.percentChange}%
+                                    </Col>
+                                </Row>
+                                <hr className='ln-white' />
+                                <Row>
+                                    <Col>
+                                        <Button onClick={this.resetCalculation}>Reset Calculation</Button>
                                     </Col>
                                 </Row>
                             </Col>
