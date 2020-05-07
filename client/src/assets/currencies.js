c0d832270ec1895d5177d29de3dcbf9ec480ddd4
mdamian9 <m.damian9@hotmail.com>
2020-05-06 20:26:28 -0700

    Created 2 separate modules, one for currency icons, and the other for the currencies themselves. Imported icons.js module into currencies.js

--- client/src/assets/currencies.js
+++ client/src/assets/currencies.js
@@ -1,40 +1,36 @@
-import USD_icon from 'cryptocurrency-icons/svg/color/usd.svg';
-import USDT_icon from 'cryptocurrency-icons/svg/color/usdt.svg';
-import BTC_icon from 'cryptocurrency-icons/svg/color/btc.svg';
-import ETH_icon from 'cryptocurrency-icons/svg/color/eth.svg';
-import BNB_icon from 'cryptocurrency-icons/svg/color/bnb.svg';
+import Icons from './icons';
 
 const Currencies = {
-    USD: {
-        name: 'United States Dollar',
-        ticker: 'USD',
-        decimals: 4,
-        icon: USD_icon
-    },
-    USDT: {
-        name: 'Tether',
-        ticker: 'USDT',
-        decimals: 7,
-        icon: USDT_icon
+    BNB: {
+        name: 'Binance Coin',
+        ticker: 'BNB',
+        decimals: 8,
+        icon: Icons.BNB
     },
     BTC: {
         name: 'Bitcoin',
         ticker: 'BTC',
         decimals: 8,
-        icon: BTC_icon
+        icon: Icons.BTC
     },
     ETH: {
         name: 'Ethereum',
         ticker: 'ETH',
         decimals: 8,
-        icon: ETH_icon
+        icon: Icons.ETH
     },
-    BNB: {
-        name: 'Binance Coin',
-        ticker: 'BNB',
-        decimals: 8,
-        icon: BNB_icon
-    }
+    USD: {
+        name: 'United States Dollar',
+        ticker: 'USD',
+        decimals: 4,
+        icon: Icons.USD
+    },
+    USDT: {
+        name: 'Tether',
+        ticker: 'USDT',
+        decimals: 7,
+        icon: Icons.USDT
+    },
 };
 
 export default Currencies;
---
+++ client/src/assets/icons.js
@@ -0,0 +1,11 @@
+import BNB_icon from 'cryptocurrency-icons/svg/color/bnb.svg';
+import BTC_icon from 'cryptocurrency-icons/svg/color/btc.svg';
+import ETH_icon from 'cryptocurrency-icons/svg/color/eth.svg';
+import USD_icon from 'cryptocurrency-icons/svg/color/usd.svg';
+import USDT_icon from 'cryptocurrency-icons/svg/color/usdt.svg';
+
+const Icons = {
+    BTC: BTC_icon, BNB: BNB_icon, ETH: ETH_icon, USD: USD_icon, USDT: USDT_icon
+};
+
+export default Icons;
