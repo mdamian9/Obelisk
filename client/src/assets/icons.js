56c98d4150c172735ad476f1b9e5aaca50b3edb1
mdamian9 <m.damian9@hotmail.com>
2020-05-08 01:18:14 -0700

    Added more cryptocurrency icons to icons.js module

--- client/src/assets/icons.js
+++ client/src/assets/icons.js
@@ -30,16 +30,67 @@ import CMT_icon from 'cryptocurrency-icons/svg/color/cmt.svg';
 import CND_icon from 'cryptocurrency-icons/svg/color/cnd.svg';
 import CVC_icon from 'cryptocurrency-icons/svg/color/cvc.svg';
 import DASH_icon from 'cryptocurrency-icons/svg/color/dash.svg';
-
-// import _icon from 'cryptocurrency-icons/svg/color/.svg';
-// import _icon from 'cryptocurrency-icons/svg/color/.svg';
-// import _icon from 'cryptocurrency-icons/svg/color/.svg';
-// import _icon from 'cryptocurrency-icons/svg/color/.svg';
-// import _icon from 'cryptocurrency-icons/svg/color/.svg';
-
+import DATA_icon from 'cryptocurrency-icons/svg/color/data.svg';
+import DCR_icon from 'cryptocurrency-icons/svg/color/dcr.svg';
+import DLT_icon from 'cryptocurrency-icons/svg/color/dlt.svg';
+import DNT_icon from 'cryptocurrency-icons/svg/color/dnt.svg';
+import DOCK_icon from 'cryptocurrency-icons/svg/color/dock.svg';
+import DOGE_icon from 'cryptocurrency-icons/svg/color/doge.svg';
+import EDO_icon from 'cryptocurrency-icons/svg/color/edo.svg';
+import ELF_icon from 'cryptocurrency-icons/svg/color/elf.svg';
+import ENG_icon from 'cryptocurrency-icons/svg/color/eng.svg';
 import ENJ_icon from 'cryptocurrency-icons/svg/color/enj.svg';
+import EOS_icon from 'cryptocurrency-icons/svg/color/eos.svg';
 import ETC_icon from 'cryptocurrency-icons/svg/color/etc.svg';
 import ETH_icon from 'cryptocurrency-icons/svg/color/eth.svg';
+import EVX_icon from 'cryptocurrency-icons/svg/color/evx.svg';
+import FUEL_icon from 'cryptocurrency-icons/svg/color/fuel.svg';
+import FUN_icon from 'cryptocurrency-icons/svg/color/fun.svg';
+import GAS_icon from 'cryptocurrency-icons/svg/color/gas.svg';
+import GNT_icon from 'cryptocurrency-icons/svg/color/gnt.svg';
+import GRS_icon from 'cryptocurrency-icons/svg/color/grs.svg';
+import GTO_icon from 'cryptocurrency-icons/svg/color/gto.svg';
+import GVT_icon from 'cryptocurrency-icons/svg/color/gvt.svg';
+import GXS_icon from 'cryptocurrency-icons/svg/color/gxs.svg';
+import HOT_icon from 'cryptocurrency-icons/svg/color/hot.svg';
+import ICX_icon from 'cryptocurrency-icons/svg/color/icx.svg';
+import INS_icon from 'cryptocurrency-icons/svg/color/ins.svg';
+import IOST_icon from 'cryptocurrency-icons/svg/color/iost.svg';
+import IOTX_icon from 'cryptocurrency-icons/svg/color/iotx.svg';
+import KMD_icon from 'cryptocurrency-icons/svg/color/kmd.svg';
+import KNC_icon from 'cryptocurrency-icons/svg/color/knc.svg';
+import LEND_icon from 'cryptocurrency-icons/svg/color/lend.svg';
+import LINK_icon from 'cryptocurrency-icons/svg/color/link.svg';
+import LOOM_icon from 'cryptocurrency-icons/svg/color/loom.svg';
+import LRC_icon from 'cryptocurrency-icons/svg/color/lrc.svg';
+import LSK_icon from 'cryptocurrency-icons/svg/color/lsk.svg';
+import LTC_icon from 'cryptocurrency-icons/svg/color/ltc.svg';
+import LUN_icon from 'cryptocurrency-icons/svg/color/lun.svg';
+import MANA_icon from 'cryptocurrency-icons/svg/color/mana.svg';
+import MATIC_icon from 'cryptocurrency-icons/svg/color/matic.svg';
+import MCO_icon from 'cryptocurrency-icons/svg/color/mco.svg';
+import MDA_icon from 'cryptocurrency-icons/svg/color/mda.svg';
+import MITH_icon from 'cryptocurrency-icons/svg/color/mith.svg';
+import MTL_icon from 'cryptocurrency-icons/svg/color/mtl.svg';
+import NANO_icon from 'cryptocurrency-icons/svg/color/nano.svg';
+import NAS_icon from 'cryptocurrency-icons/svg/color/nas.svg';
+import NAV_icon from 'cryptocurrency-icons/svg/color/nav.svg';
+import NEBL_icon from 'cryptocurrency-icons/svg/color/nebl.svg';
+import NEO_icon from 'cryptocurrency-icons/svg/color/neo.svg';
+import NULS_icon from 'cryptocurrency-icons/svg/color/nuls.svg';
+import NXS_icon from 'cryptocurrency-icons/svg/color/nxs.svg';
+import OAX_icon from 'cryptocurrency-icons/svg/color/oax.svg';
+import OMG_icon from 'cryptocurrency-icons/svg/color/omg.svg';
+import ONG_icon from 'cryptocurrency-icons/svg/color/ong.svg';
+import ONT_icon from 'cryptocurrency-icons/svg/color/ont.svg';
+import OST_icon from 'cryptocurrency-icons/svg/color/ost.svg';
+import PIVX_icon from 'cryptocurrency-icons/svg/color/pivx.svg';
+import POA_icon from 'cryptocurrency-icons/svg/color/poa.svg';
+import POE_icon from 'cryptocurrency-icons/svg/color/poe.svg';
+import POLY_icon from 'cryptocurrency-icons/svg/color/poly.svg';
+import POWR_icon from 'cryptocurrency-icons/svg/color/powr.svg';
+import PPT_icon from 'cryptocurrency-icons/svg/color/ppt.svg';
+
 import USD_icon from 'cryptocurrency-icons/svg/color/usd.svg';
 import USDT_icon from 'cryptocurrency-icons/svg/color/usdt.svg';
 
@@ -49,8 +100,17 @@ const Icons = {
     BAT: BAT_icon, BCD: BCD_icon, BCH: BCH_icon, BCPT: BCPT_icon, BEAM: BEAM_icon, BLZ: BLZ_icon, BNB: BNB_icon, BNT: BNT_icon,
     BQX: GENERIC, BRD: BRD_icon, BTC: BTC_icon, BTG: BTG_icon, BTS: BTS_icon, CDT: CDT_icon, CELR: GENERIC, CHR: GENERIC,
     CHZ: GENERIC, CMT: CMT_icon, CND: CND_icon, COS: GENERIC, COTI: GENERIC, CTSI: GENERIC, CTSX: GENERIC, CVC: CVC_icon,
-    DASH: DASH_icon,
-    ENJ: ENJ_icon, ETC: ETC_icon, ETH: ETH_icon, USD: USD_icon, USDT: USDT_icon
+    DASH: DASH_icon, DATA: DATA_icon, DCR: DCR_icon, DLT: DLT_icon, DNT: DNT_icon, DOCK: DOCK_icon, DOGE: DOGE_icon, DREP: GENERIC,
+    DUSK: GENERIC, EDO: EDO_icon, ELF: ELF_icon, ENG: ENG_icon, ENJ: ENJ_icon, EOS: EOS_icon, ERD: GENERIC, ETC: ETC_icon,
+    ETH: ETH_icon, EVX: EVX_icon, FET: GENERIC, FTM: GENERIC, FTT: GENERIC, FUEL: FUEL_icon, FUN: FUN_icon, GAS: GAS_icon,
+    GNT: GNT_icon, GO: GENERIC, GRS: GRS_icon, GTO: GTO_icon, GVT: GVT_icon, GXS: GXS_icon, HBAR: GENERIC, HC: GENERIC,
+    HIVE: GENERIC, HOT: HOT_icon, ICX: ICX_icon, INS: INS_icon, IOST: IOST_icon, IOTA: GENERIC, IOTX: IOTX_icon, KAVA: GENERIC,
+    KMD: KMD_icon, KNC: KNC_icon, LEND: LEND_icon, LINK: LINK_icon, LOOM: LOOM_icon, LRC: LRC_icon, LSK: LSK_icon, LTC: LTC_icon,
+    LTO: GENERIC, LUN: LUN_icon, MANA: MANA_icon, MATIC: MATIC_icon, MBL: GENERIC, MCO: MCO_icon, MDA: MDA_icon, MITH: MITH_icon,
+    MTL: MTL_icon, NANO: NANO_icon, NAS: NAS_icon, NAV: NAV_icon, NEBL: NEBL_icon, NEO: NEO_icon, NKN: GENERIC, NULS: NULS_icon,
+    NXS: NXS_icon, OAX: OAX_icon, OGN: GENERIC, OMG: OMG_icon, ONE: GENERIC, ONG: ONG_icon, ONT: ONT_icon, OST: OST_icon,
+    PERL: GENERIC, PHB: GENERIC, PIVX: PIVX_icon, POA: POA_icon, POE: POE_icon, POLY: POLY_icon, POWR: POWR_icon, PPT: PPT_icon,
+    USD: USD_icon, USDT: USDT_icon
 };
 
 export default Icons;
