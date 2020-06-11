13dd6d9f405306ec73d04b43206f147009bbd926
mdamian9 <m.damian9@hotmail.com>
2020-06-11 01:48:43 -0700

    Successfully implemented AlertModal to LoginForm

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import AuthService from '../AuthService/AuthService';
+import AlertModal from '../AlertModal/AlertModal';
 
 class LoginForm extends Component {
 
     constructor(props) {
         super(props);
         this.Auth = new AuthService();
+        this.state = {
+            alertModalOpen: false,
+            alertMsg: null,
+            error: false
+        };
+    };
+
+    toggleAlertModal = () => {
+        this.setState(prevState => ({ alertModalOpen: !prevState.alertModalOpen }));
     };
 
     handleChange = event => {
@@ -17,44 +27,39 @@ class LoginForm extends Component {
         });
     };
 
-    // Log in the user and redirect them to the user home page
-    handleLogin = (username, password) => {
+    handleFormSubmit = event => {
+        const { username, password } = this.state;
+        event.preventDefault();
+        event.persist();
+        // Log in the user and redirect them to the user home page
         this.Auth.login(username, password).then(() => {
             this.props.history.replace('/home');
         }).catch(err => {
-            console.log(err.response);
-            alert(err.response.data.message);
-        });
-    };
-
-    handleFormSubmit = event => {
-        event.preventDefault();
-        // Log in the user and redirect them to the user home page
-        this.handleLogin(this.state.username, this.state.password);
-        // Clear form fields
-        event.target.reset();
-        // Reset state
-        this.setState({
-            username: '',
-            password: ''
+            event.target.reset();
+            this.setState({ username: '', password: '', alertMsg: err.response.data.message, error: true });
+            this.toggleAlertModal();
         });
     };
 
     render = () => {
         return (
-            <Form onSubmit={this.handleFormSubmit}>
-                <FormGroup>
-                    <Label for='login-username'>Username:</Label>
-                    <Input type='text' name='username' id='login-username'
-                        placeholder='Enter your username' onChange={this.handleChange} required />
-                </FormGroup>
-                <FormGroup>
-                    <Label for='login-password'>Password:</Label>
-                    <Input type='password' name='password' id='login-password'
-                        placeholder='Enter your password' onChange={this.handleChange} required />
-                </FormGroup>
-                <Button color='primary'>Log in</Button>
-            </Form>
+            <div>
+                <AlertModal isOpen={this.state.alertModalOpen} toggleAlertModal={this.toggleAlertModal}
+                    message={this.state.alertMsg} error={this.state.error} reload={false} />
+                <Form onSubmit={this.handleFormSubmit}>
+                    <FormGroup>
+                        <Label for='login-username'>Username:</Label>
+                        <Input type='text' name='username' id='login-username'
+                            placeholder='Enter your username' onChange={this.handleChange} required />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='login-password'>Password:</Label>
+                        <Input type='password' name='password' id='login-password'
+                            placeholder='Enter your password' onChange={this.handleChange} required />
+                    </FormGroup>
+                    <Button color='primary'>Log in</Button>
+                </Form>
+            </div>
         );
     };
 
