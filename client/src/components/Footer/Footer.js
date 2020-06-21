8530b5169ba613fca70552ba60d29e284f393e08
mdamian9 <m.damian9@hotmail.com>
2020-06-21 02:38:06 -0700

    Worked on making LandingPage responsive. Added media queries to Footer.css and LandingPage.css / adjusted elements

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { Navbar } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from '../Logo/Logo';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <Navbar color='light' light expand='md' className='footer justify-content-center'>
-            <div href='/home' className='d-flex align-items-center' style={{ margin: '0px' }}>
+            <div id='logo' href='/home' className='d-flex align-items-center' style={{ margin: '0px' }}>
                 <Logo width='30px' height='30px' /><b>Obelisk</b>
             </div>
-            &nbsp;|&nbsp;
+            <span id='d-one'>&nbsp;|&nbsp;</span>
             <div>
-                Copyright &copy; 2020 <a href='https://linkedin.com/in/migueldamian'>Miguel Damian</a>&nbsp;|&nbsp;
+                <span id='copyright'>Copyright </span>&copy; 2020 <a href='https://linkedin.com/in/migueldamian'>Miguel Damian</a>
+            </div>
+            <span id='d-two'>&nbsp;|&nbsp;</span>
+            <div>
                 <a href='https://linkedin.com/in/migueldamian'><FontAwesomeIcon size='lg' icon={['fab', 'linkedin']} /></a>&nbsp;
                 <a href='https://github.com/mdamian9'><FontAwesomeIcon icon={['fab', 'github']} /></a>&nbsp;
-                <a href='https://migueldamian.herokuapp.com/'><FontAwesomeIcon icon='house-user' /></a>&nbsp;| All Rights Reserved
+                <a href='https://migueldamian.herokuapp.com/'><FontAwesomeIcon icon='house-user' /></a>
             </div>
+            <span id='d-three'>&nbsp;|&nbsp;</span>
+            <div id='rights'>All Rights Reserved</div>
         </Navbar>
     );
 };
