0b6dc159cc4c0f5377309933f29854f89658d9fc
mdamian9 <m.damian9@hotmail.com>
2020-06-20 22:47:00 -0700

    Working on making LandingPage responsive - created LoginModal / added to LandingPage to show Login button instead of inline form when width gets smaller

--- client/src/components/LandingNavbar/LandingNavbar.js
+++ client/src/components/LandingNavbar/LandingNavbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, Navbar, NavbarBrand, Nav } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AuthService from '../AuthService/AuthService';
 import Logo from '../Logo/Logo';
+import LoginModal from './LoginModal';
 import AlertModal from '../AlertModal/AlertModal';
 import './LandingNavbar.css';
 
@@ -54,26 +55,31 @@ class LandingNavbar extends Component {
                     <NavbarBrand id='nav-brand' href='/'>
                         <Logo width='30px' height='30px' /><b>Obelisk</b>
                     </NavbarBrand>
-                    <Nav id='nav' className='nav-class ml-auto' navbar>
-                        <Form inline id="login-form" onSubmit={this.handleFormSubmit}>
-                            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                                <Label for='login-username' className='mr-sm-2'>Username:</Label>
-                                <Input type="text"
-                                    name="username" id="login-username"
-                                    placeholder="Enter your username"
-                                    onChange={this.handleChange} required
-                                />
-                            </FormGroup>
-                            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                                <Label for='login-password' className='mr-sm-2'>Password:</Label>
-                                <Input type="password"
-                                    name="password" id="login-password"
-                                    placeholder="Enter your password"
-                                    onChange={this.handleChange} required
-                                />
-                            </FormGroup>
-                            <Button><FontAwesomeIcon icon='sign-in-alt' /> Log In</Button>
-                        </Form>
+                    <Nav className='ml-auto' navbar>
+                        <NavItem id='nav-login-form'>
+                            <Form inline id="login-form" onSubmit={this.handleFormSubmit}>
+                                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                                    <Label for='login-username' className='mr-sm-2'>Username:</Label>
+                                    <Input type="text"
+                                        name="username" id="login-username"
+                                        placeholder="Enter your username"
+                                        onChange={this.handleChange} required
+                                    />
+                                </FormGroup>
+                                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                                    <Label for='login-password' className='mr-sm-2'>Password:</Label>
+                                    <Input type="password"
+                                        name="password" id="login-password"
+                                        placeholder="Enter your password"
+                                        onChange={this.handleChange} required
+                                    />
+                                </FormGroup>
+                                <Button><FontAwesomeIcon icon='sign-in-alt' /> Log In</Button>
+                            </Form>
+                        </NavItem>
+                        <NavItem id='login-modal'>
+                            <LoginModal history={this.props.history} />
+                        </NavItem>
                     </Nav>
                 </Navbar>
             </div>
--- client/src/components/LandingNavbar/LoginModal.js
+++ client/src/components/LandingNavbar/LoginModal.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import SignupForm from '../SignupForm/SignupForm';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LoginForm from '../LoginForm/LoginForm';
+import './LandingNavbar.css';
 
-class SignupModal extends Component {
+class LoginModal extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -17,23 +20,23 @@ class SignupModal extends Component {
 
     render = () => {
         return (
-            <div>
-                <Button color='primary' onClick={this.toggleModal}>Create account</Button>
+            <div className='login-modal'>
+                <Button onClick={this.toggleModal}><FontAwesomeIcon icon='sign-in-alt' /> Log In</Button>
                 <Modal isOpen={this.state.isOpen} toggle={this.toggleModal} className={this.props.className}>
-                    <ModalHeader toggle={this.toggleModal}>Create Account</ModalHeader>
+                    <ModalHeader toggle={this.toggleModal}>Log In</ModalHeader>
                     <ModalBody>
-                        <SignupForm history={this.props.history} />
+                        <LoginForm history={this.props.history} />
                     </ModalBody>
                     <ModalFooter>
                         <div className="mx-auto">
-                            Already have an account? Log in&nbsp;<Link to="/login">here</Link>
+                            Don't have an account? Sign up <Link to='/signup'>here</Link>
                         </div>
                     </ModalFooter>
                 </Modal>
             </div>
-        )
+        );
     };
 
 };
 
-export default SignupModal;
+export default LoginModal;
